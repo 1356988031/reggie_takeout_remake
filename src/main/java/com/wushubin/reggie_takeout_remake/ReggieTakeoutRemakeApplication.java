27bba875125c0fcba2ce4fb57288c74c4b7dc338
wushubin <1356988031@qq.com>
2022-09-20 14:58:32 +0800

    瑞吉外卖项目初始化

---
+++ src/main/java/com/wushubin/reggie_takeout_remake/ReggieTakeoutRemakeApplication.java
@@ -0,0 +1,13 @@
+package com.wushubin.reggie_takeout_remake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReggieTakeoutRemakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieTakeoutRemakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wushubin/reggie_takeout_remake/ReggieTakeoutRemakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wushubin.reggie_takeout_remake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReggieTakeoutRemakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
